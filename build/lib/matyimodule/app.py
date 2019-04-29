95febdec1c78746444ae47e92bd472920c173612
MatyiFKBT <mmatyi1999@gmail.com>
2019-04-30 00:34:20 +0200

    remove

--- build/lib/matyimodule/app.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print('siker.')
-
-    
-if __name__ == '__main__':
-    main()
--- build/lib/matyimodule/matyimodule.py
+++
@@ -1,6 +0,0 @@
-from markdown import markdown
-def mark():
-    '''
-    Kiírja a kijelződre hogy ki is csinálta ezt a csomagot.
-    '''
-    return markdown(u'Matyi vagyok *cső*')
